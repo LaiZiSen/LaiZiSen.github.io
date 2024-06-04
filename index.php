2f5e5b0aec97614ada5f99108bdf8ceac93f1b2a
LaiZiSen <samlain060504>
2024-06-04 11:31:10 +0800

    testing php files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+echo '<script>console.log("Welcome to my website :D!"); </script>'; 
+?>
+
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index :>
+        </title>
+    </head>
+    <body>
+        Hello World 2 :>
+    </body>
+</html>
